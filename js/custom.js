f83821b6fdbfa7761e38f6e7ce276da725f523fa
Arthur Dias <117912903+Arthur-Diasz@users.noreply.github.com>
2023-06-20 22:40:09 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,9 @@
 
     // HERO SLIDE
     $('.hero-slide').backstretch([
-      "images/slideshow/white-wall-living-room-have-sofa-decoration-3d-rendering.jpg", 
-      "images/slideshow/interior-wall-mockup-with-sofa-cabinet-living-room-with-empty-white-wall-background-3d-rendering.jpg",
-      "images/slideshow/wood-sideboard-living-room-interior-with-copy-space.jpg"
+      "img/exclusive/03.jpg", 
+      "img/exclusive/05.jpg",
+      "img/exclusive/06.jpg"
     ],  {duration: 2000, fade: 750});
 
     // REVIEWS CAROUSEL
